4fcf37ba629dd3b0b993b5eda9486611661f8563
IDGS-802-21000485 <21000485@alumnos.utleon.edu.mx>
2025-03-26 08:27:19 -0600

    Diseño corregido

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
 import Login from "./pages/Login";
 import Usuarios from "./pages/Usuarios";
 
@@ -8,7 +9,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/usuarios" element={<Usuarios />} />
+        <Route element={<MainLayout />}>
+          <Route path="/usuarios" element={<Usuarios />} />
+        </Route>
       </Routes>
     </Router>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';  // Bootstrap
 import './index.css'; // Estilos globales
+import './App.css'; // Estilos de la aplicación
+import 'bootstrap/dist/css/bootstrap.min.css';  // Bootstrap
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
